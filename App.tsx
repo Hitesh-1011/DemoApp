44aa2eb7cdf50786ada1d206bb212e16bad24f2f
Hitesh <hitesh@Hiteshs-MacBook-Air.local>
2025-10-21 10:52:27 +0530

    apptsx fixed

--- App.tsx
+++ App.tsx
@@ -5,11 +5,9 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 function App() {
@@ -24,14 +22,9 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={{color: 'white'}}>Hello</Text>
     </View>
   );
 }
@@ -39,6 +32,9 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
   },
 });
 
